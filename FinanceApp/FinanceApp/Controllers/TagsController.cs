63f3ca212d16e9c370b608e27eaabd1ba00c7586
Lusey77 <Lusey77@users.noreply.github.com>
2017-11-12 17:39:57 -0600

    Added Controllers

---
+++ FinanceApp/FinanceApp/Controllers/TagsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Tags")]
+    public class TagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
